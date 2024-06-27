02b3743e9d9505b25e9f7477397f17ed26796b61
Rubi Anggoro <rubi.anggoro199@gmail.com>
2024-06-28 01:02:48 +0700

    Init shadcn-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import "./index.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-red-500">Hello World</h1>
+      <Button variant={"destructive"} size={"sm"}>
+        Halo
+      </Button>
     </div>
   );
 }
