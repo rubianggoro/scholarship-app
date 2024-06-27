a600502cff134c0059073e4be185bd75893e730f
Rubi Anggoro <rubi.anggoro199@gmail.com>
2024-06-28 00:41:45 +0700

    init backend

---
+++ server/src/Schema/Mutations/User.ts
@@ -0,0 +1,65 @@
+import { GraphQLID, GraphQLString } from "graphql";
+import { UserType } from "../TypeDefs/User";
+import { Users } from "../../Entities/Users";
+import { MessageType } from "../TypeDefs/Messages";
+
+export const CREATE_USER = {
+  type: UserType,
+  args: {
+    name: { type: GraphQLString },
+    username: { type: GraphQLString },
+    password: { type: GraphQLString },
+  },
+  async resolve(parent: any, args: any) {
+    const { name, username, password } = args;
+    await Users.insert({
+      name,
+      username,
+      password,
+    });
+    return args;
+  },
+};
+
+export const UPDATE_PASSWORD = {
+  type: MessageType,
+  args: {
+    username: { type: GraphQLString },
+    oldPassword: { type: GraphQLString },
+    newPassword: { type: GraphQLString },
+  },
+  async resolve(parent: any, args: any) {
+    const { username, oldPassword, newPassword } = args;
+    const user = await Users.findOne({ where: { username: username } });
+    if (!user) {
+      throw new Error("USERNAME DOESNT EXIST");
+    }
+    const userPassword = user?.password;
+
+    if (oldPassword === userPassword) {
+      await Users.update({ username: username }, { password: newPassword });
+      return {
+        success: true,
+        message: "PASSWORD UPDATED",
+      };
+    } else {
+      throw new Error("PASSWORDS DO NOT MATCH!");
+    }
+  },
+};
+
+export const DELETE_USER = {
+  type: MessageType,
+  args: {
+    id: { type: GraphQLID },
+  },
+  async resolve(parent: any, args: any) {
+    const id = args.id;
+    await Users.delete(id);
+
+    return {
+      success: true,
+      message: "DELETE USER SUCCESS",
+    };
+  },
+};
---
+++ server/src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import { graphqlHTTP } from "express-graphql";
+import cors from "cors";
+import { schema } from "./Schema";
+import { createConnection } from "typeorm";
+import { Users } from "./Entities/Users";
+
+const main = async () => {
+  await createConnection({
+    type: "mysql",
+    database: "graphqlcrud",
+    username: "root",
+    password: "",
+    logging: true,
+    synchronize: false,
+    entities: [Users],
+  });
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+    })
+  );
+
+  app.listen(3001, () => {
+    console.log("SERVER RUNNING ON PORT 3001");
+  });
+};
+
+main().catch((err) => {
+  console.log(err);
+});
