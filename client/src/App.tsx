bb942fa331d03ea62f0b8003a326b3c1a732188b
Rubi Anggoro <rubi.anggoro199@gmail.com>
2024-06-28 08:05:18 +0700

    init some components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,83 @@
 import React from "react";
 import "./index.css";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { Label } from "./components/ui/label";
+import { Input } from "./components/ui/input";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "./components/ui/form";
 import { Button } from "./components/ui/button";
+import { useToast } from "./components/ui/use-toast";
+import { Toaster } from "./components/ui/toaster";
+
+const FormSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be",
+  }),
+});
 
 function App() {
+  const { toast } = useToast();
+
+  const client = new ApolloClient({
+    uri: "http://localhost:3001/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log("data", data);
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
   return (
-    <div className="App">
-      <h1 className="text-red-500">Hello World</h1>
-      <Button variant={"destructive"} size={"sm"}>
-        Halo
-      </Button>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="flex w-full justify-center items-center min-h-screen">
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="w-52 space-y-6"
+          >
+            <FormField
+              control={form.control}
+              name="username"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="shadcn" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button type="submit">Submit</Button>
+          </form>
+        </Form>
+        <Toaster />
+      </div>
+    </ApolloProvider>
   );
 }
 
